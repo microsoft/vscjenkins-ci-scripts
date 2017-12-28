2bcbcfb10121b25b77cd6c9ebf62dbbc5fc135f0
Raphael Yu <xianyu@microsoft.com>
2017-12-28 17:39:26 +0800

    Delete unused files.

--- vars/runSanityChecks.groovy
+++
@@ -1,9 +0,0 @@
-#!groovy
-import com.microsoft.azure.devops.ci.utils
-
-def call(nodeLabel) {
-    def ciUtils = new com.microsoft.azure.devops.ci.utils()
-    node(nodeLabel) {
-        echo 'works'
-    }
-}
